91a0a22d4120e12dd04a3a442618e8b4e889b5ee
Pierre-Yves Landuré <pierre-yves.landure@isen-ouest.yncrea.fr>
2021-12-08 12:43:40 +0100

    Formatted and checked with pylint.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+"""Trombinoscope package setup.py file.
+   """
 from distutils.core import setup
 
 setup(
-   name='Trombinoscope', 
-   version='0.1dev',
-   description="A simple facebook for Simplon",
-   author="Pierre-Yves Landuré, Kevin BOUCLY",
-   author_email="pierre-yves.landure@isen-ouest.yncrea.fr, kevin.boucly@isen-ouest.yncrea.fr",
-   packages=['trombinoscope',],
-   license='GPLv3', 
-   long_description=open('README.md').read(),
+    name="Trombinoscope",
+    version="0.1dev",
+    description="A simple facebook for Simplon",
+    author="Pierre-Yves Landuré, Kevin BOUCLY",
+    author_email="pierre-yves.landure@isen-ouest.yncrea.fr, kevin.boucly@isen-ouest.yncrea.fr",
+    packages=["trombinoscope"],
+    license="GPLv3",
+    long_description=open("README.md").read(),
 )
